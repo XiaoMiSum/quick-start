79bfdea630e34aaac344a8c3ab45f6c4f4b0993b
xiaomi <mi_xiao@qq.com>
2025-04-08 11:33:59 +0800

    :sparkles: 定时任务更新评审\计划状态

--- quick-click-server/src/main/java/io/github/xiaomisum/quickclick/service/qualitycenter/plan/PlanServiceImpl.java
+++ quick-click-server/src/main/java/io/github/xiaomisum/quickclick/service/qualitycenter/plan/PlanServiceImpl.java
@@ -103,23 +103,25 @@ public class PlanServiceImpl implements PlanService {
 
     @Scheduled(cron = CRON)
     public void scheduleUpdateStatus() {
-        mapper.selectByStatus(Preparing).forEach(plan -> {
-            List<PlanCase> cases = planCaseMapper.selectList(plan.getId());
+        mapper.selectByStatus(Preparing).forEach(item -> {
+            List<PlanCase> cases = planCaseMapper.selectList(item.getId());
             Map<TestStatus, List<PlanCase>> group = cases.stream()
                     .collect(Collectors.groupingBy(PlanCase::getResult));
-            List<PlanCase> prepareList = Preparing.get(group);
-            List<PlanCase> passList = Passed.get(group);
-            List<PlanCase> failureList = Failed.get(group);
-            List<PlanCase> blockingList = Blocking.get(group);
-            List<PlanCase> skipList = Skipped.get(group);
-            List<PlanCase> underwayList = Processing.get(group);
-            if (prepareList.isEmpty() && underwayList.isEmpty()) {
-                // 进行中和未开始的都为空，评审完成
-                mapper.updateStatus(plan.getId(), Finished);
-            } else if ((passList.size() + failureList.size() +
-                    blockingList.size() + skipList.size() + underwayList.size()) < cases.size()) {
-                // 成功数量 + 失败数量 + 阻塞数量 + 跳过数量 + 进行中数量 之和 小于 总数，评审进行中
-                mapper.updateStatus(plan.getId(), Processing);
+            List<PlanCase> preparing = Preparing.get(group);
+            List<PlanCase> passed = Passed.get(group);
+            List<PlanCase> failed = Failed.get(group);
+            List<PlanCase> blocking = Blocking.get(group);
+            List<PlanCase> skipped = Skipped.get(group);
+            List<PlanCase> processing = Processing.get(group);
+            if (preparing.isEmpty() && processing.isEmpty()) {
+                // 进行中和未开始的都为空，执行完成
+                mapper.updateStatus(item.getId(), Finished);
+            } else if (preparing.size() == cases.size()) {
+                // 准备中的数量与规划的测试用例总数一致，准备中
+                mapper.updateStatus(item.getId(), Preparing);
+            } else if ((passed.size() + failed.size() + blocking.size() + skipped.size() + processing.size()) < cases.size()) {
+                // 成功数量 + 失败数量 + 阻塞数量 + 跳过数量 + 进行中数量 之和 小于 总数，执行进行中
+                mapper.updateStatus(item.getId(), Processing);
             }
         });
     }
--- quick-click-server/src/main/java/io/github/xiaomisum/quickclick/service/qualitycenter/review/ReviewServiceImpl.java
+++ quick-click-server/src/main/java/io/github/xiaomisum/quickclick/service/qualitycenter/review/ReviewServiceImpl.java
@@ -103,23 +103,25 @@ public class ReviewServiceImpl implements ReviewService {
 
     @Scheduled(cron = CRON)
     public void scheduleUpdateStatus() {
-        mapper.selectByStatus(Preparing).forEach(review -> {
-            List<ReviewCase> cases = reviewCaseMapper.selectList(review.getId());
+        mapper.selectByStatus(Preparing).forEach(item -> {
+            List<ReviewCase> cases = reviewCaseMapper.selectList(item.getId());
             Map<TestStatus, List<ReviewCase>> group = cases.stream()
                     .collect(Collectors.groupingBy(ReviewCase::getResult));
-            List<ReviewCase> prepareList = Preparing.get(group);
-            List<ReviewCase> passList = Passed.get(group);
-            List<ReviewCase> failureList = Failed.get(group);
-            List<ReviewCase> blockingList = Blocking.get(group);
-            List<ReviewCase> skipList = Skipped.get(group);
-            List<ReviewCase> underwayList = Processing.get(group);
-            if (prepareList.isEmpty() && underwayList.isEmpty()) {
+            List<ReviewCase> preparing = Preparing.get(group);
+            List<ReviewCase> passed = Passed.get(group);
+            List<ReviewCase> failed = Failed.get(group);
+            List<ReviewCase> blocking = Blocking.get(group);
+            List<ReviewCase> skipped = Skipped.get(group);
+            List<ReviewCase> processing = Processing.get(group);
+            if (preparing.isEmpty() && processing.isEmpty()) {
                 // 进行中和未开始的都为空，评审完成
-                mapper.updateStatus(review.getId(), Finished);
-            } else if ((passList.size() + failureList.size() +
-                    blockingList.size() + skipList.size() + underwayList.size()) < cases.size()) {
+                mapper.updateStatus(item.getId(), Finished);
+            } else if (preparing.size() == cases.size()) {
+                // 准备中的数量与规划的测试用例总数一致，准备中
+                mapper.updateStatus(item.getId(), Preparing);
+            } else if ((passed.size() + failed.size() + blocking.size() + skipped.size() + processing.size()) < cases.size()) {
                 // 成功数量 + 失败数量 + 阻塞数量 + 跳过数量 + 进行中数量 之和 小于 总数，评审进行中
-                mapper.updateStatus(review.getId(), Processing);
+                mapper.updateStatus(item.getId(), Processing);
             }
         });
     }
